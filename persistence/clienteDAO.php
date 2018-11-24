37f52abec75f3768091fcf7e3df6829f50151df3
Vinicius Spinelli <viinicius.sg@hotmail.com>
2018-11-24 16:15:08 -0200

    Cadastro de clientes no banco com verificação de cpf e emails cadastrados funcionando.

--- persistence/clienteDAO.php
+++ persistence/clienteDAO.php
@@ -3,10 +3,23 @@
 
 	class ClienteDAO {
 		function cadastrar($cliente, $link) {
+			/* conferir se o email ou cpf já foi cadastrado */
+			$queryCPF = mysqli_query($link, "SELECT * FROM clientes WHERE cpf= '".($cliente->getCpf())."'");
+			$queryCPFRows = mysqli_num_rows($queryCPF);
+			$queryEmail = mysqli_query($link, "SELECT email FROM clientes 
+									 WHERE email= '".($cliente->getEmail())."'");
+			$queryEmailRows = mysqli_num_rows($queryEmail);
+
+			if ($queryCPFRows > "0") {
+				die("O CPF já foi cadastrado na PeVStore.");
+			}
+			if ($queryEmailRows > "0") {
+				die("O Email já foi cadastrado na PeVStore.");
+			}
+
 			$query = "INSERT INTO clientes (nome, nascimento, cpf, email, cep, endereco, cidade, telefone, senha) values ('".($cliente->getNome())."','".($cliente->getNascimento())."',
 				'".($cliente->getCpf())."','".($cliente->getEmail())."','".($cliente->getCep())."',
-				'".($cliente->getEndereco())."','".($cliente->getCidade())."','".($cliente->getTelefone())."',
-				'".($cliente->getSenha())."')";
+				'".($cliente->getEndereco())."','".($cliente->getCidade())."','".($cliente->getTelefone())."','".($cliente->getSenha())."')";
 
 			if(!mysqli_query($link, $query)) {
 				die("Erro ao tentar se cadastrar!.");
