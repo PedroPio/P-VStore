5eeb31cc655378c925d3e59a13d24636254b0555
PedroPio <pedropio935@gmail.com>
2018-12-03 14:30:56 -0200

    Banco de dados atualizado

--- pevstore.sql
+++ pevstore.sql
@@ -1,64 +1,148 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+-- -----------------------------------------------------
+-- Schema pevstore
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema pevstore
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `pevstore` DEFAULT CHARACTER SET utf8 ;
 USE `pevstore` ;
 
-CREATE TABLE IF NOT EXISTS `pevstore`.`clientes` (
-  `idCliente` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `nascimento` DATE NOT NULL,
+-- -----------------------------------------------------
+-- Table `pevstore`.`Pessoa`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pevstore`.`Pessoa` (
   `cpf` VARCHAR(14) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `cep` VARCHAR(9) NOT NULL,
-  `endereco` VARCHAR(100) NOT NULL,
-  `cidade` VARCHAR(45) NOT NULL,
-  `telefone` VARCHAR(14) NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `dataNascimento` VARCHAR(8) NULL,
+  `email` VARCHAR(200) NOT NULL,
+  `eAdmin` TINYINT(1) NOT NULL,
   `senha` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idCliente`))
+  PRIMARY KEY (`cpf`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `pevstore`.`produtos` (
-  `idProduto` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `marca` VARCHAR(45) NOT NULL,
-  `fornecedor` VARCHAR(45) NOT NULL,
-  `descricao` VARCHAR(100) NOT NULL,
-  `preco` DECIMAL(8,2) NOT NULL,
-  `quantidade` INT NOT NULL,
-  PRIMARY KEY (`idProduto`))
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Transportadora`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pevstore`.`Transportadora` (
+  `CNPJ` VARCHAR(14) NOT NULL,
+  `nome` VARCHAR(45) NULL,
+  PRIMARY KEY (`CNPJ`))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `pevstore`.`transportadora` (
-  `idTransportadora` INT NOT NULL,
-  `nome` VARCHAR(45) NOT NULL,
-  `cnpj` VARCHAR(18) NOT NULL,
-  PRIMARY KEY (`idTransportadora`))
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Carrinho`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pevstore`.`Carrinho` (
+  `idCarrinho` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idCarrinho`))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `pevstore`.`compras` (
-  `produtos_idProduto` INT NOT NULL,
-  `clientes_idCliente` INT NOT NULL,
-  `transportadora_idTransportadora` INT NOT NULL,
-  `protocolo` INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`protocolo`),
-  CONSTRAINT `fk_compras_produtos`
-    FOREIGN KEY (`produtos_idProduto`)
-    REFERENCES `pevstore`.`produtos` (`idProduto`)
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Pedido`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pevstore`.`Pedido` (
+  `numPedido` VARCHAR(10) NOT NULL,
+  `dataPedido` DATE NOT NULL,
+  `Pessoa_cpf` VARCHAR(14) NOT NULL,
+  `Transportadora_CNPJ` VARCHAR(14) NOT NULL,
+  `Carrinho_idCarrinho` INT NOT NULL,
+  PRIMARY KEY (`numPedido`, `Pessoa_cpf`, `Transportadora_CNPJ`, `Carrinho_idCarrinho`),
+  CONSTRAINT `fk_Pedido_Pessoa`
+    FOREIGN KEY (`Pessoa_cpf`)
+    REFERENCES `pevstore`.`Pessoa` (`cpf`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_compras_clientes1`
-    FOREIGN KEY (`clientes_idCliente`)
-    REFERENCES `pevstore`.`clientes` (`idCliente`)
+  CONSTRAINT `fk_Pedido_Transportadora1`
+    FOREIGN KEY (`Transportadora_CNPJ`)
+    REFERENCES `pevstore`.`Transportadora` (`CNPJ`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_compras_transportadora1`
-    FOREIGN KEY (`transportadora_idTransportadora`)
-    REFERENCES `pevstore`.`transportadora` (`idTransportadora`)
+  CONSTRAINT `fk_Pedido_Carrinho1`
+    FOREIGN KEY (`Carrinho_idCarrinho`)
+    REFERENCES `pevstore`.`Carrinho` (`idCarrinho`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Pagamento`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pevstore`.`Pagamento` (
+  `idPagamento` INT NULL AUTO_INCREMENT,
+  `tipoPagamento` VARCHAR(45) NOT NULL,
+  `numParcelas` INT NOT NULL,
+  `Pedido_numPedido` VARCHAR(10) NOT NULL,
+  `Pedido_Pessoa_cpf` VARCHAR(14) NOT NULL,
+  `Pedido_Transportadora_CNPJ` VARCHAR(14) NOT NULL,
+  `Pedido_Carrinho_idCarrinho` INT NOT NULL,
+  PRIMARY KEY (`idPagamento`, `Pedido_numPedido`, `Pedido_Pessoa_cpf`, `Pedido_Transportadora_CNPJ`, `Pedido_Carrinho_idCarrinho`),
+  CONSTRAINT `fk_Pagamento_Pedido1`
+    FOREIGN KEY (`Pedido_numPedido` , `Pedido_Pessoa_cpf` , `Pedido_Transportadora_CNPJ` , `Pedido_Carrinho_idCarrinho`)
+    REFERENCES `pevstore`.`Pedido` (`numPedido` , `Pessoa_cpf` , `Transportadora_CNPJ` , `Carrinho_idCarrinho`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Produto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pevstore`.`Produto` (
+  `codProduto` INT NOT NULL AUTO_INCREMENT,
+  `precoCompra` DECIMAL(8,2) NULL,
+  `precoVenda` DECIMAL(8,2) NULL,
+  `Pessoa_cpf` VARCHAR(14) NOT NULL,
+  PRIMARY KEY (`codProduto`, `Pessoa_cpf`),
+  CONSTRAINT `fk_Produto_Pessoa1`
+    FOREIGN KEY (`Pessoa_cpf`)
+    REFERENCES `pevstore`.`Pessoa` (`cpf`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Fornecedor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pevstore`.`Fornecedor` (
+  `cnpjFornecedor` VARCHAR(14) NOT NULL,
+  `nome` VARCHAR(80) NOT NULL,
+  PRIMARY KEY (`cnpjFornecedor`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Produto_has_Fornecedor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pevstore`.`Produto_has_Fornecedor` (
+  `Produto_codProduto` INT NOT NULL,
+  `Produto_Pessoa_cpf` VARCHAR(14) NOT NULL,
+  `Fornecedor_cnpjFornecedor` VARCHAR(14) NOT NULL,
+  PRIMARY KEY (`Produto_codProduto`, `Produto_Pessoa_cpf`, `Fornecedor_cnpjFornecedor`),
+  CONSTRAINT `fk_Produto_has_Fornecedor_Produto1`
+    FOREIGN KEY (`Produto_codProduto` , `Produto_Pessoa_cpf`)
+    REFERENCES `pevstore`.`Produto` (`codProduto` , `Pessoa_cpf`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Produto_has_Fornecedor_Fornecedor1`
+    FOREIGN KEY (`Fornecedor_cnpjFornecedor`)
+    REFERENCES `pevstore`.`Fornecedor` (`cnpjFornecedor`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
