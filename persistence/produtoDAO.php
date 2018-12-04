19a78256ac022b3163a2008f0f7d9e27de95ec90
PedroPio <pedropio935@gmail.com>
2018-12-04 20:48:06 -0200

    Adicionou transportadora no banco de dados

--- persistence/produtoDAO.php
+++ persistence/produtoDAO.php
@@ -3,7 +3,7 @@
 
 	class ProdutoDAO {
 		function cadastrar($produto, $link) {
-			$query = "INSERT INTO Produto (nome, precoCompra, precoVenda, quantidade, idFornecedor) values ('".($produto->getNome())."','".($produto->getPrecoCompra())."','".($produto->getPrecoVenda())."','".($produto->getQuantidade())."', '".($produto->getidFornecedor())."')";
+			$query = "INSERT INTO Produto (nome, precoCompra, precoVenda, idFornecedor,qtd) values ('".($produto->getNome())."','".($produto->getPrecoCompra())."','".($produto->getPrecoVenda())."','".($produto->getidFornecedor())."', '".($produto->getQuantidade())."')";
 			echo $query;
 			if(!mysqli_query($link, $query)) {
 				die("Erro ao tentar cadastrar produto!");
@@ -31,7 +31,7 @@
 				$result = mysqli_fetch_array($query);
 				echo '<div class="col-md-4">';
 				echo 	'<div class="product-at">';
-				echo 		'<a href="single.html">';
+				echo 		'<a href="single.php">';
 				echo 			'<img class="img-responsive" src="../images/pi3.jpg" alt="">';
 				echo 			'<div class="pro-grid">';
 				echo 				'<span class="buy-in">Comprar</span>';
@@ -39,7 +39,7 @@
 				echo 		'</a>';
 				echo 	'</div>';
 				echo 	'<div style="text-align: center; padding-top: 15px; padding-bottom: 15px"><b>'.$result['nome'].'</b></div>';
-				echo 	'<a class="item_add" href="checkout.php?acao=add&idProduto='.$result['idProduto'].'"><p class="number item_price"><i> </i>R$ '.$result['preco'].'</p></a>';
+				echo 	'<a class="item_add" href="checkout.php?acao=add&idProduto='.$result['idProduto'].'"><p class="number item_price"><i> </i>R$ '.$result['precoVenda'].'</p></a>';
 				echo 	'<div style="padding-bottom: 70px"></div>';
 				echo '</div>';
 			}
--- pevstore.sql
+++ pevstore.sql
@@ -1,10 +1,22 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `pevstore` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema pevstore
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema pevstore
+-- -----------------------------------------------------
+CREATE DATABASE IF NOT EXISTS `pevstore` DEFAULT CHARACTER SET utf8 ;
 USE `pevstore` ;
 
+-- -----------------------------------------------------
+-- Table `pevstore`.`Pessoa`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pevstore`.`Pessoa` (
   `cpf` VARCHAR(14) NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
@@ -13,43 +25,60 @@ CREATE TABLE IF NOT EXISTS `pevstore`.`Pessoa` (
   `eAdmin` TINYINT(1) NULL DEFAULT 0,
   `senha` VARCHAR(45) NOT NULL,
   `id` INT NOT NULL AUTO_INCREMENT,
-  UNIQUE INDEX (`email`) ,
+  UNIQUE INDEX (`email`),
   PRIMARY KEY (`id`),
   UNIQUE INDEX (`cpf`))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `pevstore`.`Compra` (
-  `idCarrinho` VARCHAR(10) NOT NULL,
-  `dataPedido` DATE NOT NULL,
-  `Pessoa_id` INT NOT NULL,
-  PRIMARY KEY (`idCarrinho`, `Pessoa_id`))
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Transportadora`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pevstore`.`Transportadora` (
+  `idTransportadora` INT NOT NULL AUTO_INCREMENT,
+  `cnpjTransportadora` VARCHAR(45) NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idTransportadora`),
+  UNIQUE INDEX  (`cnpjTransportadora`))
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Fornecedor`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pevstore`.`Fornecedor` (
   `cnpjFornecedor` VARCHAR(14) NOT NULL,
   `nome` VARCHAR(80) NOT NULL,
   `idFornecedor` INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`idFornecedor`),
-  UNIQUE INDEX  (`cnpjFornecedor`))
+  UNIQUE INDEX (`cnpjFornecedor`))
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Produto`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pevstore`.`Produto` (
-  `nome` VARCHAR(45) NOT NULL,
   `codProduto` INT NOT NULL AUTO_INCREMENT,
   `precoCompra` DECIMAL(8,2) NULL,
   `precoVenda` DECIMAL(8,2) NULL,
-  `Fornecedor_idFornecedor` INT NOT NULL,
+  `idFornecedor` INT NOT NULL,
   `qtd` INT NOT NULL,
-  PRIMARY KEY (`codProduto`, `Fornecedor_idFornecedor`))
+  `nome` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`codProduto`))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `pevstore`.`Carrinho` (
-  `Produto_codProduto` INT NOT NULL,
-  `Produto_Fornecedor_idFornecedor` INT NOT NULL,
-  `Carrinho_idCarrinho` VARCHAR(10) NOT NULL,
-  `Carrinho_Pessoa_id` INT NOT NULL,
-  `qtd` INT NOT NULL,
-  PRIMARY KEY (`Produto_codProduto`, `Produto_Fornecedor_idFornecedor`, `Carrinho_idCarrinho`, `Carrinho_Pessoa_id`))
+
+-- -----------------------------------------------------
+-- Table `pevstore`.`Compra`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pevstore`.`Compra` (
+  `idCompra` INT NOT NULL AUTO_INCREMENT,
+  `dataPedido` DATE NOT NULL,
+  `Pessoa_id` INT NOT NULL,
+  `idTransportadora` INT NOT NULL,
+  `codProduto` INT NOT NULL,
+  PRIMARY KEY (`idCompra`))
 ENGINE = InnoDB;
 
 
